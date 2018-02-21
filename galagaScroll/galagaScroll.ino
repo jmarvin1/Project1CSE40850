5c9998b05ad85d3a9c3ffa73c41a3a8ab0e054f1
J. Patrick Lacher <jlacher1@nd.edu>
2018-02-21 18:13:43 -0500

    Initial Commit

---
+++ galagaScroll/galagaScroll.ino
@@ -0,0 +1,54 @@
+#include <Arduboy2.h>
+
+Arduboy2 ab;
+
+const unsigned char background[] PROGMEM = {
+  0x00, 0x00, 0x6, 0x4, 0x00, 0x00, 0x00, 0x40, 0x60, 0x00, 0x2, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x38, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x6, 0x6, 0x00, 0x00, 0x00, 0x00,
+};
+
+const unsigned char player [] PROGMEM = {
+  0x00, 0x2, 0x26, 0xae, 0xfe, 0xfa, 0xf2, 0x72, 0x52, 0xd0, 0xd0, 0x90, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x81, 0xc9, 0xeb, 0xff, 0xbf, 0x9f, 0x9d, 0x94, 0x16, 0x17, 0x13, 0x3, 0x1, 0x1, 0x1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+int playerx = 5;
+int playery = 10;
+int scroll = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  ab.begin();
+  ab.clear();
+  ab.setFrameRate(60);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Pause Renderer Between Frames
+  if (!ab.nextFrame()) return;
+  
+  ab.clear();
+
+  // Draw Sprites
+  scroll = (scroll+1)%16;
+  for (int bgx = 16-scroll; bgx<128; bgx += 16) {
+    for (int bgy = 0; bgy < 64; bgy += 16) {
+      ab.drawBitmap(bgx,bgy,background, 8, 8, WHITE);
+    }
+  }
+  ab.fillRect(playerx,playery,17,17,BLACK);
+  ab.drawBitmap(playerx,playery,player,17,17,WHITE);
+  
+  // Move Player Sprite
+  if (ab.pressed(LEFT_BUTTON)) playerx--;
+  if (ab.pressed(RIGHT_BUTTON)) playerx++;
+  if (ab.pressed(UP_BUTTON)) playery--;
+  if (ab.pressed(DOWN_BUTTON)) playery++;
+
+  // Collision Detection With Edge of Screen
+  if (playerx < 0) playerx=0;
+  if (playerx > 111) playerx = 111;
+  if (playery < 0) playery = 0;
+  if (playery > 47) playery = 47;
+
+  ab.display();
+}
